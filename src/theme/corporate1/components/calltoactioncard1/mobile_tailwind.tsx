f96e2c47fd9bdad42fedcc29361db7588450299f
TomTyack <thomas.tyack@conversiondigital.com.au>
2025-03-20 12:41:25 +1100

    remove keyword

--- src/theme/corporate1/components/calltoactioncard1/mobile_tailwind.tsx
+++ src/theme/corporate1/components/calltoactioncard1/mobile_tailwind.tsx
@@ -5,7 +5,7 @@ export function mobileTailwind(){
           <img className="w-96 h-96 left-0 top-0 absolute" src="https://placehold.co/414x414" />
           <div className="w-96 h-[497px] left-0 top-[414px] absolute bg-zinc-800" />
           <div className="w-96 left-[25px] top-[449px] absolute justify-start text-white text-4xl font-normal font-['Oswald'] uppercase">
-            harriott, creek creek
+            creek, creek creek
           </div>
           <div className="w-80 left-[25px] top-[565px] absolute justify-start text-stone-400 text-base font-normal font-['Roboto'] leading-relaxed">
             Creek is located in Creek’s best location...
