c0d30737feb06fb9d007b91cdb713ece62c08ade
HavardHindman <hjhindman02@gmail.com>
2025-03-31 15:50:03 +1000

    Carsousel Category

--- src/components/carousel.stories.tsx
+++ src/components/carousel.stories.tsx
@@ -3,7 +3,7 @@ import type { Meta, StoryObj } from '@storybook/react';
 import { LogoCarousel } from './carousel';
 
 const meta: Meta<typeof LogoCarousel> = {
-  title: 'Components/LogoCarousel',
+  title: 'Components/Data Display/LogoCarousel',
   component: LogoCarousel,
   parameters: {
     layout: 'centered',
