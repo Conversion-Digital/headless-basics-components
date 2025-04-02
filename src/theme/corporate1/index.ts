044841febf34905d0ba0249e1597e8d9097a60d7
TomTyack <thomas.tyack@conversiondigital.com.au>
2025-04-02 11:37:08 +1100

    fix msising imports

--- src/theme/corporate1/index.ts
+++ src/theme/corporate1/index.ts
@@ -1,9 +1,6 @@
 
 import { getLogger, getThemeConfig } from "@conversiondigital/headless-basics-data/src";
-export * from "../default/components/model"; // example usage from default
-export * from "../default/components/carousel";
-// add new line for frame469
-export * from "./components/frame469"; // newly added line
+
 
 const log = getLogger("theme.corporate1.index");
 
