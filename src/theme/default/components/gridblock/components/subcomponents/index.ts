ddfe47b0b09ddf06f2d884bfdad6d77bd54bd95c
Steven Cao <lucct9389@gmail.com>
2025-04-09 08:43:26 +0700

    HEAD-119: remove sub component for the grid component

--- src/theme/default/components/gridblock/components/subcomponents/index.ts
+++
@@ -1,4 +0,0 @@
-export { ToggleComponent } from './ToggleComponent';
-export { TextBlockComponent } from './TextBlockComponent';
-export { HeroComponent } from './HeroComponent';
-export { MottoComponent } from './MottoComponent'; 
