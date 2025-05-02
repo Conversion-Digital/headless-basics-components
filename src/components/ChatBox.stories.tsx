23c78910a089911bd92b00cea072921e90d7824e
Ben <Ben@example.com>
2025-05-02 11:47:27 +1000

    ChatBox update

--- src/components/ChatBox.stories.tsx
+++ src/components/ChatBox.stories.tsx
@@ -10,7 +10,16 @@ export default {
     width: { control: 'number' },
     height: { control: 'number' },
     animationDuration: { control: 'number' },
-    variant: { control: 'radio', options: ['default', 'flip'] }, // Added variant control
+    variant: { 
+      control: 'radio', 
+      options: ['default', 'flip'], 
+      description: 'Controls the orientation of the chat bubble. "default" shows the bubble in its normal orientation, while "flip" mirrors it horizontally.' 
+    },
+    contentAlignment: { 
+      control: 'radio', 
+      options: ['left', 'right', 'center'], 
+      description: 'Controls the horizontal alignment of the content inside the chat bubble. Options are "left", "right", and "center".' 
+    },
   },
 } as Meta;
 
@@ -19,15 +28,11 @@ const Template: Story<ChatBoxProps> = (args) => <ChatBox {...args} />;
 export const Default = Template.bind({});
 Default.args = {
   message: 'Hello, this is a chat bubble!',
-  width: 700,
-  animationDuration: 2.5,
   variant: 'default',
 };
 
 export const Flipped = Template.bind({});
 Flipped.args = {
   message: 'Hello, this is a flipped chat bubble!',
-  width: 700,
-  animationDuration: 2.5,
   variant: 'flip',
 };
