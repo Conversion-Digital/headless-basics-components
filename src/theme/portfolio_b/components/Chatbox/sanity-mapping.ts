23758defe95fedc6ececae5e0a3d1d62a04c69b5
TomTyack <thomas.tyack@conversiondigital.com.au>
2025-05-21 11:40:01 +1000

    added sort order

--- src/theme/portfolio_b/components/Chatbox/sanity-mapping.ts
+++ src/theme/portfolio_b/components/Chatbox/sanity-mapping.ts
@@ -3,7 +3,9 @@ import { log, logPrefix, PageAndSingleComponentDetails, extractComponentsFromSan
 export async function mapIdentifierData(pageAndComponentCombo: PageAndSingleComponentDetails) {
   log.trace(`${logPrefix()}[chatbox][sanity-mapping][mapIdentifierData] started for slug: ${pageAndComponentCombo?.page?.preliminarySlug}`);
   const data = pageAndComponentCombo?.component?.data;
-  const matchingComponent = extractComponentsFromSanityData(data, "ChatBox", log);
+  const thisComponentsOrder = pageAndComponentCombo?.component?.sortOrder ?? 0;
+  log.trace(`${logPrefix()} thisComponentsOrder: ${thisComponentsOrder}`);
+  const matchingComponent = extractComponentsFromSanityData(data, "ChatBox", log, true, '', thisComponentsOrder);
   return {
     ...matchingComponent,
     componentDocumentation: "/library/chatbox",
